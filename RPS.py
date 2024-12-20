bf38a0a5a6c4f6f5660e1ef2b34adc526357cb4d
Davide <82649871+davidecarluccio@users.noreply.github.com>
2024-12-20 10:10:03 +0100

    Provided my solution

--- RPS.py
+++ RPS.py
@@ -1,10 +1,37 @@
 # The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
 
-def player(prev_play, opponent_history=[]):
-    opponent_history.append(prev_play)
+import random
 
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+def player(prev_play, prob_matr = [{}], key = [""], guess = [""], counter = [0]):
+    # 
+    response = {"P": "S", "S": "R", "R": "P"} 
 
-    return guess
+    
+    if prev_play == "" or counter[0] > 70:  # configurable memory parameter (counter)
+        prev_play = random.choice(["R", "P", "S"])
+        guess[0] = random.choice(["R", "P", "S"])
+        counter[0] = 0
+        
+        prob_matr[0] = {"RR": {"R": 0, "P": 0, "S": 0},
+                     "RP": {"R": 0, "P": 0, "S": 0},
+                     "RS": {"R": 0, "P": 0, "S": 0},
+                     "PR": {"R": 0, "P": 0, "S": 0},
+                     "PP": {"R": 0, "P": 0, "S": 0},
+                     "PS": {"R": 0, "P": 0, "S": 0},
+                     "SR": {"R": 0, "P": 0, "S": 0},
+                     "SP": {"R": 0, "P": 0, "S": 0},
+                     "SS": {"R": 0, "P": 0, "S": 0},       
+            } # zeroing the pobability matrix
+  
+    
+    counter[0] += 1
+
+    if key[0] != "":
+        prob_matr[0][key[0]][prev_play] += 1 # updating values of probability matrix
+
+
+    key[0] = guess[0] + prev_play
+    prediction = max(prob_matr[0][key[0]], key = lambda k: prob_matr[0][key[0]][k])
+    guess[0] = response[prediction]
+
+    return guess[0]
